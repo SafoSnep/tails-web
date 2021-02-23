21679dc1e31cff95de86861db665ae63193341f3
SafoSnep <45212701+SafoSnep@users.noreply.github.com>
2021-02-23 17:27:36 +0200

    Alert iOS <14 users about minimum required version

--- js/dl.js
+++ js/dl.js
@@ -5,13 +5,13 @@ function checkVersion() {
     var ua = window.navigator.userAgent;
     if (-1 < ua.indexOf("iPhone OS")) {
       var ver = ua.match(/\b(\d+)_(\d+)(?:_(\d+))?\b/);
-      return 4 === ver.length && 13 <= parseInt(ver[1]);
+      return 4 === ver.length && 14 <= parseInt(ver[1]);
     }
   }
   return !0;
 }
 
 function checkVersionAndDownload(e) {
-  if (!checkVersion()) return e.preventDefault(), oldVersionWarned = !0, alert("iOS 13 or newer is required. You may download the .ipa by tapping the download button again, and try to install it, but you're on your own."), 
+  if (!checkVersion()) return e.preventDefault(), oldVersionWarned = !0, alert("iOS 14 or newer is required. You may try the install anyway by tapping the button again, but you're on your own."), 
   !1;
 }
